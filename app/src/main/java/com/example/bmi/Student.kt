33ee072516fe592b420f757033d2d94dffc5be14
kiwi <davincidx0418@gmail.com>
2022-03-15 11:58:18 +0800

    constructor #2

--- app/src/main/java/com/example/bmi/Student.kt
+++ app/src/main/java/com/example/bmi/Student.kt
@@ -1,9 +1,13 @@
 package com.example.bmi
 
-class Student constructor(
-    var name: String,
+class Student(var name: String,
     var english: Int,
     var math: Int){
+    //null constructor
+    constructor(): this("", 0,0){
+        println("Hi")
+    }
+    constructor(name: String): this(name,0,0)
     //Fields
     fun print(){
         println("$name\t$english\t$math\t${average()}")
@@ -13,6 +17,7 @@ class Student constructor(
     }
 
     fun main(){
+        val hank = Student("Hank")
         val jack = Student("Jack", 85, 65)
         jack.print()
     }
